b226ff47f7afad13b0a1e360645691068200a804
Shaun Pimenta <shaunpimenta607@gmail.com>
2022-10-22 00:12:09 +0530

    A new Beginning

---
+++ static/assets/js/test.js
@@ -0,0 +1,47 @@
+var survey = []; //Bidimensional array: [ [1,3], [2,4] ]
+
+//Switcher function:
+$(".rb-tab").click(function(){
+  //Spot switcher:
+  $(this).parent().find(".rb-tab").removeClass("rb-tab-active");
+  $(this).addClass("rb-tab-active");
+});
+
+//Save data:
+$(".trigger").click(function(){
+  //Empty array:
+  survey = [];
+  //Push data:
+  for (i=1; i<=$(".rb").length; i++) {
+    var rb = "rb" + i;
+    var rbValue = parseInt($("#rb-"+i).find(".rb-tab-active").attr("data-value"));
+    //Bidimensional array push:
+    survey.push(rbValue); //Bidimensional array: [ [1,3], [2,4] ]
+  }
+  //Debug:
+  console.log(survey);
+  $.ajax({
+    type: "POST",
+    url: "/inter",
+    data: JSON.stringify(survey),
+    ContentType: "application/json"
+        // success: function(response)
+        // {
+        //     $("#result").html(response);
+        // },
+        // error: function(response)
+        // {
+        //     console.log(response);
+        // }    
+  });
+  // debug();
+});
+
+//Debug:
+function debug(){
+  var debug = "";
+  for (i=0; i<survey.length; i++) {
+    debug += "Nº " + survey[i][0] + " = " + survey[i][1] + "\n";
+  };
+  alert(debug);
+};
